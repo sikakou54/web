7bce82b21fd5e3cc54371f84884376481e19a1e0
sikakou <sikakou0504@outlook.jp>
2020-09-14 22:56:53 +0900

    新規追加 -投稿アプリを作成

---
+++ web/app1/bbs_data.py
@@ -0,0 +1,34 @@
+import os, json, datetime
+
+#保存先ファイル
+BASE_DIR = os.path.dirname(__file__)
+SAVE_FILE = BASE_DIR + '/data/log.json'
+
+#ログ(JSON)ファイルを読み出す
+def load_data():
+    print(SAVE_FILE)
+    if not os.path.exists(SAVE_FILE):
+        print('none file')
+        return
+    print('find file')
+    with open( SAVE_FILE, 'rt', encoding='utf-8') as f:
+        print('open file')
+        return json.load( f )
+
+#ログ(JSON)ファイルに書き込む
+def save_data( data_list ):
+    with open(SAVE_FILE, 'wt', encoding='utf-8') as f:
+        json.dump( data_list, f )
+
+#ログ(JSON)ファイルに追記保存
+def save_data_append( user, text ):
+    tm = get_datatime_now()
+    data = { 'name':user, 'text':text, 'date':tm }
+    data_list = load_data()
+    data_list.insert( 0, data )
+    save_data( data_list )
+
+#日時を文字列で得る
+def get_datatime_now():
+    now = datetime.datetime.now()
+    return "{0:%Y%m%d %H%M}".format( now )
---
+++ web/app1/bbs_login.py
@@ -0,0 +1,30 @@
+from flask import session, redirect
+
+USERLIST = {
+    'user1':'aaa',
+    'user2':'bbb',
+    'user3':'ccc',
+}
+
+def is_login():
+    print('is_login')
+    return 'login' in session
+
+
+def try_login( user, pw ):
+    if user not in USERLIST:
+        return False
+    if USERLIST[user] != pw:
+        return False
+    session['login'] = user
+    return True
+
+def try_logout():
+    session.pop( 'login', None )
+    return True
+
+def get_user():
+    if is_login():
+        print('get_user')
+        return session['login']
+    return'not login'
