638a7f91c2a4800dc699dc13d0e903785350e0fa
sikakou <sikakou0504@outlook.jp>
2020-10-07 18:52:00 +0900

    静的ファイルを定義

---
+++ node-web-project/App-P2P-multi/index.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+var path = require('path');
+
+// https://socket.io/docs/#Using-with-Express
+const app = express();
+const server = http.Server(app);
+
+// '/socket.io/socket.io.js' でクライアント用の JavaScript ファイルが取得できるようになる
+const io = socketIo(server);  
+
+// 静的ファイルのパスを指定する
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  console.log('[/]');
+  res.sendFile(__dirname + '/index.html');
+});
+
+let port = 3000;
+
+server.listen(port, () => {
+  console.log('Server Started : ' + port );
+});
+
+io.on('connection', (socket) => {
+  console.log('A User Connected');
+  
+  socket.on('message', (message) => {
+    console.log('On Message', message); 
+    socket.broadcast.emit('message', message);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('User Disconnected');
+  });
+});
